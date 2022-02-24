d70acdcb974374df4b92e662f8e94a465386700d
ixnp <enprocopios@gmail.com>
2022-02-24 09:20:38 -0800

    solution

--- game-review-api/app/models/review.rb
+++ game-review-api/app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
- 
+    belongs_to :game
+    belongs_to :user
+    validates :review, length: {minimum: 5, maximum: 5} 
+
 end
--- game-review-api/app/models/user.rb
+++ game-review-api/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-   
+    has_many :reviews
+    has_many :games, through: :reviews
+
+    validates :username, presence: true, unniqueness: true
 end
+
